7a4f05ed9efebc30347f64b6cfe2a37c5e679525
abubaker <mohammad_abubaker@yahoo.com>
2021-12-03 13:15:32 +0500

    Working...

--- app/src/main/java/org/abubaker/palletdemo/MainActivity.kt
+++ app/src/main/java/org/abubaker/palletdemo/MainActivity.kt
@@ -21,17 +21,20 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        // viewBinding = Inflating XML Layout using viewBinding
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        // Load the image from a random link from google.
+        // Load the image from a random link from google using the Glide Library.
         Glide.with(this)
 
-            // To check the error you can change the link by adding some char
+            // Image Source: To check the error you can change the link by adding some char
             .load("https://cdn.pixabay.com/photo/2018/05/03/21/49/android-3372580_960_720.png")
 
-            // Add the listener
+            // Add the listener: Once the image will be downloaded then take following action:
             .listener(object : RequestListener<Drawable> {
+
+                // Failure: Log the Error message if image will be failed to download.
                 override fun onLoadFailed(
                     @Nullable e: GlideException?,
                     model: Any?,
@@ -47,6 +50,7 @@ class MainActivity : AppCompatActivity() {
 
                 }
 
+                // Success: Load time thumbnail in our ImageView
                 override fun onResourceReady(
                     resource: Drawable,
                     model: Any?,
@@ -61,7 +65,11 @@ class MainActivity : AppCompatActivity() {
 
                             // Get the vibrantSwatch color from the image and set it to the parent layout background.
                             palette?.let {
+
+                                // Store the RGB value instead the intColor variable
                                 val intColor = it.vibrantSwatch?.rgb ?: 0
+
+                                // Change background color based on the fetched RGB value
                                 binding.targetTheme.setBackgroundColor(intColor)
                             }
 
@@ -71,6 +79,7 @@ class MainActivity : AppCompatActivity() {
                 }
             })
 
+            // Load image into the ImageView
             .into(binding.imgReference)
 
     }
